1f4dfcc5307f70f850f1ed79547bf47bdd8c0978
Shane Yost <scy@shaneyost.io>
2025-05-19 21:12:33 -0600

    updated logger w/ required lvl args

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,19 @@ int main()
     int i (0);
     while(1)
     {
-        LOG_WARN("This is a warn log w/ no fmt string");
-        LOG_INFO("This is a info log w/ no fmt string");
-        LOG_DEBUG("This is a debug log w/ no fmt string");
-        LOG_WARN("This is a warn log w/ fmt string (%s) %02X", "hi", i);
-        LOG_INFO("This is a info log w/ fmt string (%s) %02X", "hi", i);
-        LOG_DEBUG("This is a debug log w/ fmt string (%s) %02X", "hi", i);
+        LOG(Logger::Level::WARN, "This is a warn log w/ no fmt string");
+        LOG(Logger::Level::INFO, "This is a info log w/ no fmt string");
+        LOG(Logger::Level::DEBUG, "This is a debug log w/ no fmt string");
+
+        LOG(Logger::Level::WARN, "This is a warn log w/ fmt string (%s) %02X", "hi", i);
+        LOG(Logger::Level::INFO, "This is a info log w/ fmt string (%s) %02X", "hi", i);
+        LOG(Logger::Level::DEBUG, "This is a debug log w/ fmt string (%s) %02X", "hi", i);
+
         i+=1;
+
         std::vector<uint8_t> data = {1,2,3,4,5};
-        LOG_VECTOR_WARN(data, data.size());
+        LOG_VECTOR(Logger::Level::INFO, data, data.size());
+
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
     }
